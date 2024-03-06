3effc7456cce5a2cacfc12932babb9f96aa11a39
Rafi Gabizon <109092985+RafiGabizon@users.noreply.github.com>
2024-03-06 20:55:10 +0200

    Add files via upload

---
+++ models/drinkModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+let schema = new mongoose.Schema({
+  name: String,
+  cals: Number,
+  price: Number,
+  img_url: String,
+  user_id:String
+})
+exports.DrinkModel = mongoose.model("drinks", schema)
+
+exports.validateDrink = (_reqBody) => {
+  let joiSchema = Joi.object({
+    name: Joi.string().min(2).max(100).required(),
+    cals: Joi.number().min(1).max(999).required(),
+    price: Joi.number().min(1).max(999).required(),
+    img_url: Joi.string().min(2).max(400).allow(null, ""),
+  })
+  return joiSchema.validate(_reqBody)
+}
+
+
